b44e1d84f21b2e904c30cd84e1c8baeb555dc8bb
Ruslan Popesku <ruslanpopesku95@gmail.com>
2023-05-30 17:35:45 +0300

    Initial Commit

---
+++ WhiteNoise/WhiteNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteNoiseApp.swift
+//  WhiteNoise
+//
+//  Created by Ruslan Popesku on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhiteNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
